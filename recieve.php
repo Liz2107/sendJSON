f113910797cd31f4a98a81fb837bf6923ae35d05
Liz2107 <escutting@gmail.com>
2022-05-01 20:58:57 -0600

    basic CSS and php file writing

--- recieve.php
+++ recieve.php
@@ -1,7 +1,8 @@
 ?<php
 $requestPayload = file_get_contents("php://input");
 $myfile = fopen("results.txt", "w") or die("Unable to open file!");
-$txt = $requestPayload + \n";
+$data = json_decode($requestPayload);
+$txt = $data + \n";
 fwrite($myfile, $txt);
 fclose($myfile);
 >?
