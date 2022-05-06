b7ed5e6f3fcf99ad0185b9f2520b3ba60caa3a2f
Liz2107 <escutting@gmail.com>
2022-05-05 19:51:55 -0600

    updates to php

--- recieve.php
+++ recieve.php
@@ -1,8 +1,10 @@
 ?<php
 $requestPayload = file_get_contents("php://input");
 $myfile = fopen("results.txt", "w") or die("Unable to open file!");
+echo "<script>console.log("php line 4");</script>";
 $data = json_decode($requestPayload);
 $txt = $data + \n";
 fwrite($myfile, $txt);
 fclose($myfile);
+echo "<script>console.log("php line 8");</script>";
 >?
